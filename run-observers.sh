886bd043c8a53ec0e8d5597b7acc0918ce770a08
xXHachimanXx <globaldep90@gmail.com>
2021-12-18 11:00:29 -0300

    Adding first files

---
+++ run-observers.sh
@@ -0,0 +1,7 @@
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/grafana.yaml;
+
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/jaeger.yaml;
+
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/kiali.yaml;
+
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/prometheus.yaml;
