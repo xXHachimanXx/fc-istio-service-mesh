7fdfd83cb495df58f4a31d843774b954e564fb92
xXHachimanXx <globaldep90@gmail.com>
2021-12-19 18:53:57 -0300

    Adding fortio deploy

--- run-observers.sh
+++ run-observers.sh
@@ -5,3 +5,7 @@ kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samp
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/kiali.yaml;
 
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/prometheus.yaml;
+
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/httpbin/sample-client/fortio-deploy.yaml;
+
+export FORTIO_POD=$(kubectl get pods -l app=fortio -o 'jsonpath={.items[0].metadata.name}');
