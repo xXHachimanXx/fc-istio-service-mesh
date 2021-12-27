6a0007beeca3d32ecc67ec36b0feb46b121d41bb
xXHachimanXx <globaldep90@gmail.com>
2021-12-27 18:15:26 -0300

    Hands on with Circuit Breaker

--- run-fortio.sh
+++ run-fortio.sh
@@ -1,5 +1,5 @@
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/httpbin/sample-client/fortio-deploy.yaml \;
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/httpbin/sample-client/fortio-deploy.yaml;
 
-export FORTIO_POD=$(kubectl get pods -l app=fortio -o 'jsonpath={.items[0].metadata.name}') \;
+export FORTIO_POD=$(kubectl get pods -l app=fortio -o 'jsonpath={.items[0].metadata.name}');
 
-kubectl exec "$FORTIO_POD" -c fortio -- fortio load -c 2 -qps 0 -t 200s -loglevel Warning http://nginx-service:8080 \;
+kubectl exec "$FORTIO_POD" -c fortio -- fortio load -c 2 -qps 0 -t 200s -loglevel Warning http://nginx-service:8080
